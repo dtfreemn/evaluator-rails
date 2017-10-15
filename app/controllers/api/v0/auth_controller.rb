2275649efbbb734e08a81ad716bc806522c404b8
Tim Freeman <d.timothy.freeman@gmail.com>
2017-10-15 14:11:17 -0400

    added password authentication to administrator login

--- app/controllers/api/v0/auth_controller.rb
+++ app/controllers/api/v0/auth_controller.rb
@@ -2,7 +2,7 @@ class Api::V0::AuthController < ApplicationController
 
   def create
     admin = Administrator.include_all.find_by(email: params[:email])
-    if admin
+    if admin && admin.authenticate(params[:password])
       token = encode_token({administrator_id: admin.id})
       render json: {admin: admin.as_json(include_hash), jwt: token}
     else
