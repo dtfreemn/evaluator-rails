56a64439ff1293d66100b1ade9d32c0dd273769b
Tim Freeman <d.timothy.freeman@gmail.com>
2017-11-03 14:05:30 -0400

    updated seeds and added new heroku url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://cryptic-savannah-42632.herokuapp.com'
+    origins 'https://evaluationmanager.herokuapp.com'
 
     resource '*',
       headers: :any,
